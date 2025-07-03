7f52a5b1963ba533cd0ca01ec5b2a3da3f62c0c3
Gauravkr0220 <gauravkr0220@gmail.com>
2025-07-03 11:29:34 +0530

    frontend

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Navbar from "./Navbar"
+import Layout from "./layout/Layout"
+import Notloggedin from './layout/Notloggedin';
+import MessageBox from './layout/MessageBox';
+
+
+const Home = () => {
+
+  
+  return (
+    <Layout>
+      
+        <MessageBox/>
+      
+    </Layout>
+  );
+}
+
+export default Home;
